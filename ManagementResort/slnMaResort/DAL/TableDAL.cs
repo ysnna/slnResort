e343629c5b38555bc54f9c7a219640e959dcb985
lehongdanh99 <lehongdanh99@gmail.com>
2020-07-12 18:41:50 +0700

    update DAL

--- ManagementResort/slnMaResort/DAL/TableDAL.cs
+++ ManagementResort/slnMaResort/DAL/TableDAL.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace slnMaResort.DAL
 {
-    class TableDAL
+    public class TableDAL
     {
         private static TableDAL instance;
         private TableDAL() { }
