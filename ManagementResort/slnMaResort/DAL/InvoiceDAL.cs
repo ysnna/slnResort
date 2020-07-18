393adcff48fe7da366e64898cc9ddc1cad1b88bd
vuonglestudent <vuongvaba1v2@gmail.com>
2020-07-18 12:56:36 +0700

    create InvoiceDAL

---
+++ ManagementResort/slnMaResort/DAL/InvoiceDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slnMaResort.DAL
+{
+    class InvoiceDAL
+    {
+        private static InvoiceDAL instance;
+        private InvoiceDAL() { }
+
+        internal static InvoiceDAL Instance 
+        {
+            get
+            {
+                if (instance == null) instance = new InvoiceDAL();
+                return InvoiceDAL.instance;
+            }
+            private set => InvoiceDAL.instance = value;
+        }
+
+
+    }
+}
